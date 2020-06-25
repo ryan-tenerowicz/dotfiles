5373ef322471d0ef1018c36e31340fdf55ce273a
ryan-tenerowicz <github@ryantenerowicz.dev>
2020-06-25 11:49:16 -0700

    tvix

--- update.sh
+++ update.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 cp -r ~/.config/i3 .
-cp -r ~/.config/i3status .
+#cp -r ~/.config/i3status .
 cp -r ~/.config/i3blocks .
 
-cp ~/.vimrc .
-cp -r ~/.vim .
-rm -rf ./.vim/bundle/
+#cp ~/.vimrc .
+#cp -r ~/.vim .
+#rm -rf ./.vim/bundle/
 cp -r ~/.config/nvim .
 rm -rf ./nvim/plugins/
 
-cp ~/.bashrc .
-cp ~/.inputrc .
-cp ~/.bash_aliases .
+#cp ~/.bashrc .
+#cp ~/.inputrc .
+#cp ~/.bash_aliases .
 cp ~/.zshrc .
 cp -r ~/.zsh .
 rm ./.zsh/.cwd
 
-cp ~/.config/libinput-gestures.conf .
-cp ~/.config/redshift.conf .
+#cp ~/.config/libinput-gestures.conf .
+#cp ~/.config/redshift.conf .
 cp ~/.xbindkeysrc .
 cp -r ~/.config/background .
 cp -r ~/.scripts .
